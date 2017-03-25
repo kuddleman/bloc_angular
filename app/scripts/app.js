eae169397379ce406a0ec144a29584d5c7313462
Donny <EDL1155@gmail.com>
2017-03-25 09:20:56 -0700

    extra commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,14 +8,20 @@ function config($stateProvider, $locationProvider) {
 	$stateProvider
          .state('landing', {
              url: '/',
+		     controller: 'LandingCtrl as landing',
              templateUrl: '/templates/landing.html'
          })
 	
 	      .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
-         });
-
+         })
+          .state('collection', {
+             url: '/collection',
+		     controller: 'CollectionCtrl as collection',
+             templateUrl: '/templates/collection.html'
+         });    
+	
  }
 
 (function() {
